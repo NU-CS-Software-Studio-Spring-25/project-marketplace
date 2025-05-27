4ba868db609e0f1a201f919dfedb09da2d9738f0
Jovy <zjjovyy8215@gmail.com>
2025-05-27 00:01:23 -0500

    Fix CORS error

--- marketplace-backend/config/initializers/cors.rb
+++ marketplace-backend/config/initializers/cors.rb
@@ -1,10 +1,14 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://nu-cs-course-marketplace-a7681a168efe.herokuapp.com', 
+            'http://localhost:3000', 
+            'http://localhost:5000', 
+            'https://localhost:3000',
+            'https://localhost:5000'
     
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
       credentials: true
   end
-end 
+end
