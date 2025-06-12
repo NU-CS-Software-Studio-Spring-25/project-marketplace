bcd20aabb894a08da01574133179ef5e54a3d757
hma423 <henryma2026@u.northwestern.edu>
2025-06-11 20:48:27 -0500

    reocmmendation systme working

--- marketplace-backend/app/models/label.rb
+++ marketplace-backend/app/models/label.rb
@@ -1,3 +1,4 @@
 class Label < ApplicationRecord
     has_and_belongs_to_many :courses
+    has_and_belongs_to_many :users, join_table: :user_labels
 end
--- marketplace-backend/app/models/user.rb
+++ marketplace-backend/app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
     # has_secure_password
     has_many :enrollments
     has_many :courses, through: :enrollments
+    has_and_belongs_to_many :labels, join_table: :user_labels
 end
